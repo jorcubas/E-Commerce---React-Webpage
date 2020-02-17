32d63779925f844d90e108f7444338bfddf19e9f
Cubas <jorcubas22@gmail.com>
2020-02-17 15:09:29 -0600

    Implement firabase utils, included ability to store authenticated users to our database.

--- crwn-clothing/src/firebase/firebase.utils.js
+++ crwn-clothing/src/firebase/firebase.utils.js
@@ -13,6 +13,37 @@ const config = {
     measurementId: "G-Z172JJXFPS"
   };
 
+  export const createUserProfileDocument = async(userAuth, additionalData) => {
+    if(!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+      const {displayName, email} = userAuth;
+      console.log(displayName);
+      console.log(email);
+      const createdAt = new Date();
+
+      try{
+        await userRef.set({
+          displayName, 
+          email, 
+          createdAt,
+          ...additionalData
+        })
+      } catch (error){
+        console.log('error creating user', error.message);
+      }
+    }
+
+    return userRef;
+
+  };
+
+
+
   firebase.initializeApp(config);
 
   export const auth = firebase.auth();
