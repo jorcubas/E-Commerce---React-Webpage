c8cda4a0a4145652f6ff2200c29a27dd9a59953b
Cubas <jorcubas22@gmail.com>
2020-02-19 02:56:22 -0600

    Updating App to Redirect To Home if user is signed in, also types for user reduce have been modified (no more hard coded strings).

--- crwn-clothing/src/redux/user/user.actions.js
+++ crwn-clothing/src/redux/user/user.actions.js
@@ -1,5 +1,7 @@
+import {UserActionTypes} from './user.types';
+
 export const setCurrentUser = user => ({
     // consts should always have snake case
-    type: 'SET_CURRENT_USER',
+    type: UserActionTypes.SET_CURRENT_USER,
     payload: user
 });
