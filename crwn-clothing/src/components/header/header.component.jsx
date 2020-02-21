66ce33f403ed38ea42864f4789658664632966b6
Cubas <jorcubas22@gmail.com>
2020-02-20 18:36:15 -0600

    Added Checkout Page, Checkout item component and updated cart to use selector from reselect.

--- crwn-clothing/src/components/header/header.component.jsx
+++ crwn-clothing/src/components/header/header.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
 import {auth} from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropwdown from '../cart-dropdown/cart-dropdown.component';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
 
 // React Component Tells Render SVG file.
 import { ReactComponent as Logo } from '../../assets/crown.svg';
@@ -35,14 +38,14 @@ const Header = ({currentUser, hidden}) => (
         </div>
         {hidden ? null : <CartDropwdown/>}
     </div>
-)
+);
 
 //  Way to destructurr de value in this case of the user 
 // in the state
 
-const mapStateToProps = ({user: {currentUser}, cart: {hidden} }) => ({
-    currentUser, 
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser, 
+    hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
